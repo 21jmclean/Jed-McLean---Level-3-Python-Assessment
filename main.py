2dcc2d4695fc4e69548e4284010a51763f0132f9
21jmclean <21jmclean@wakatipu.school.nz>
2025-05-02 11:44:00 +1200

    Made the basic structure of the progrm uncluder the main class and the support class plus the main routine.

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+from tkinter import *
+debtors = []
 
+class Debt:
+    def __init__(self, name, amount, date, reason):
+        self.name = name
+        self.amount = amount
+        self.date = date
+        self.reason = reason
+
+    def get_details(self):
+        return f"{self.name}: ${self.amount}, {self.date}, {self.reason}"
+    
+    
+class GUI:
+    def __init__(self, parent):
+        self.parent = parent
+        main_page = Frame(root)
+        t1 = Label(text="Debtor:")
+        for i in range(len(debtors)):
+            debtor = Label(main_page, text = "f{}")
+            debt = Label()
+
+        
+
+if __name__ == "__main__":
+    root = Tk()
+    app = GUI(root)
+    root.mainloop()
